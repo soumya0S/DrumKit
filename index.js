3ae19167b873138557f4066dce7d4a24ff8d32eb
soumya0S <87618650+soumya0S@users.noreply.github.com>
2021-12-09 22:25:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+for (var index = 0; index < numberOfDrums; index++) {
+    document.querySelectorAll(".drum")[index].addEventListener('click', function (event) {
+       makeSound(this.innerHTML)
+    });
+}
+document.addEventListener('keydown', function (event) {
+   makeSound(event.key);
+});
+function makeSound(key){
+switch (key) {
+    case "w":
+        var audio0 = new Audio("sounds/tom-1.mp3");
+        audio0.play();
+        break;
+    case "a":
+        var audio1 = new Audio("sounds/tom-2.mp3");
+        audio1.play();
+        break;
+    case "s":
+        var audio2 = new Audio("sounds/tom-3.mp3");
+        audio2.play();
+        break;
+    case "d":
+        var audio3 = new Audio("sounds/tom-4.mp3");
+        audio3.play();
+        break;
+    case "j":
+        var audio4 = new Audio("sounds/snare.mp3");
+        audio4.play();
+        break;
+    case "k":
+        var audio5 = new Audio("sounds/crash.mp3");
+        audio5.play();
+        break;
+    case "l":
+        var audio6 = new Audio("sounds/kick-bass.mp3");
+        audio6.play();
+        break;
+
+    default:
+        break;
+}
+}
+ 
